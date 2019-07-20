7ff7039995085fed824fd55b16d02c354fef6d9e
lukm4nz3r0 <laruku.asep@gmail.com>
2019-07-20 13:38:27 +0700

    first commit

--- URL.js
+++ URL.js
@@ -1,5 +1,5 @@
-const realURL = 'nodejs-sql-socketio.herokuapp.com'
-// const realURL = '192.168.100.22:3000'
+
+const realURL = '192.168.100.22:3000'
 
 export const SocketIoURL = 'wss://'+realURL
 
